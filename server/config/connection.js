e721d28140f8252f58a03717508c63ea830410e2
Clay_MM <clay.moyer.1515@gmail.com>
2023-06-29 20:35:00 -0400

    correcting branches

--- server/config/connection.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONGODB_URI || 'mongodb:', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
-
-module.exports = mongoose.connection;
