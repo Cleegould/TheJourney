b8954c1092b5f87140970b014a78016c3c4b7614
chrisg <cleegould@gmail.com>
2023-07-02 16:38:32 -0400

    updated journal page

--- client/src/pages/journal.js
+++ client/src/pages/journal.js
@@ -1,13 +1,16 @@
 import React, { useState } from "react";
-import { TextField, Button, Box, Typography, IconButton, Dialog, DialogTitle, DialogContent, DialogActions } from "@mui/material";
-import { format } from "date-fns";
+import { TextField, Button, Box, Typography, IconButton, LinearProgress } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
+import journeyLogo from "../images/journey.jpg";
+
+import '../../src/assets/css/JournalEntry.css'
 
 const JournalEntry = () => {
   const [entry, setEntry] = useState("");
   const [entries, setEntries] = useState([]);
-  const [deleteIndex, setDeleteIndex] = useState(null);
-  const [isDeleteDialogOpen, setIsDeleteDialogOpen] = useState(false);
+  const lastLoggedIn = "June 25, 2023"; // Placeholder value, replace with actual last login date
+  const challengesCompleted = 10; // Placeholder value, replace with actual number
+  const challengePercent = 0.5; // Placeholder value, replace with actual progress percentage as decimal (0 to 1)
 
   // Function to handle changes in the journal entry text
   const handleEntryChange = (event) => {
@@ -17,7 +20,7 @@ const JournalEntry = () => {
   // Function to handle saving the journal entry
   const handleSaveEntry = () => {
     // Get the current timestamp
-    const timestamp = format(new Date(), "yyyy-MM-dd HH:mm:ss");
+    const timestamp = new Date().toLocaleString();
 
     // Save the journal entry and timestamp
     const newEntry = { entry, timestamp };
@@ -29,23 +32,9 @@ const JournalEntry = () => {
 
   // Function to handle deleting a journal entry
   const handleDeleteEntry = (index) => {
-    setDeleteIndex(index);
-    setIsDeleteDialogOpen(true);
-  };
-
-  // Function to confirm the deletion of a journal entry
-  const confirmDeleteEntry = () => {
     const updatedEntries = [...entries];
-    updatedEntries.splice(deleteIndex, 1);
+    updatedEntries.splice(index, 1);
     setEntries(updatedEntries);
-    setDeleteIndex(null);
-    setIsDeleteDialogOpen(false);
-  };
-
-  // Function to cancel the deletion of a journal entry
-  const cancelDeleteEntry = () => {
-    setDeleteIndex(null);
-    setIsDeleteDialogOpen(false);
   };
 
   return (
@@ -57,11 +46,28 @@ const JournalEntry = () => {
       height="100vh"
       padding={2}
       textAlign="center"
-    >
-      <Typography variant="h4" gutterBottom>
+      bgcolor="#FE5720" /* Background color */
+      color="#000009" /* Text color */
+    > <img src={journeyLogo} alt="the journey logo"/>
+     
+      <Typography variant="h3" gutterBottom style={{ fontFamily: "Papyrus" }}>
         My Journal
       </Typography>
+      
       <Box width={400}>
+        <Typography variant="h5" gutterBottom style={{ fontFamily: "Papyrus" }}>
+          Last Logged In: {lastLoggedIn}
+        </Typography>
+        <Typography variant="h5" gutterBottom style={{ fontFamily: "Papyrus" }}>
+          Challenges Completed: {challengesCompleted}
+        </Typography>
+        Current Challenge Progress
+        <LinearProgress
+          className="progress-bar"
+          variant="determinate"
+          value={challengePercent * 100}
+          style={{ backgroundColor: "#000009",}} /* Progress bar color */
+        />
         <TextField
           label="Journal Entry"
           multiline
@@ -71,8 +77,17 @@ const JournalEntry = () => {
           fullWidth
           margin="normal"
         />
-        <Button variant="contained" onClick={handleSaveEntry} fullWidth>
-          Save
+        <Button
+          variant="contained"
+          onClick={handleSaveEntry}
+          fullWidth
+          style={{
+            fontFamily: "Papyrus",
+            backgroundColor: "#E5AB24", /* Button color */
+            color: "#556062" /* Text color */
+          }}
+        >
+          Save Entry
         </Button>
       </Box>
       <Box
@@ -83,7 +98,9 @@ const JournalEntry = () => {
         justifyContent="center"
         alignItems="center"
       >
-        <Typography variant="h6">Journal Entries:</Typography>
+        <Typography variant="h6" style={{ fontFamily: "Papyrus" }}>
+          Journal Entries:
+        </Typography>
         {entries.map((entry, index) => (
           <Box
             key={index}
@@ -105,24 +122,6 @@ const JournalEntry = () => {
           </Box>
         ))}
       </Box>
-      <Dialog
-        open={isDeleteDialogOpen}
-        onClose={cancelDeleteEntry}
-        maxWidth="xs"
-      >
-        <DialogTitle>Delete Journal Entry</DialogTitle>
-        <DialogContent>
-          <Typography variant="body1">
-            Are you sure you want to delete this journal entry?
-          </Typography>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={cancelDeleteEntry}>Cancel</Button>
-          <Button onClick={confirmDeleteEntry} color="error">
-            Delete
-          </Button>
-        </DialogActions>
-      </Dialog>
     </Box>
   );
 };
