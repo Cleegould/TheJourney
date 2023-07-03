325c9b0b0240a480a091e53bfe7ab91e36f8e491
Jack Killeen <j31killeen@gmail.com>
2023-07-03 13:31:03 -0400

    added auth middlewear to context

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-
 const {ApolloServer} = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
 
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection')
@@ -8,13 +8,18 @@ const db = require('./config/connection')
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
     typeDefs,
-    resolvers
+    resolvers,
+    context: authMiddleware
 });
 
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
 
 const startApolloServer = async (typedefs, resolvers) => {
     await server.start();
