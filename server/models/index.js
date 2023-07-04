4ce3c299c95eed29808d04abf6aa5d237952d5a3
Clay_MM <clay.moyer.1515@gmail.com>
2023-07-03 23:36:51 -0400

    models fix

--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,6 @@
 // Imports and Exports the challenge, challenge progress and user variables;
 const Challenge = require('./challenge');
-const User = require ('./User');
+const User = require ('./user');
+const Task = require('./task')
 
-module.exports = { Challenge, User };
+module.exports = { Challenge, User, Task };
