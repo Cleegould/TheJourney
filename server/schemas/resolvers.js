1bd6f46525837d288b8de81213c96d192da05512
Clay_MM <clay.moyer.1515@gmail.com>
2023-07-03 23:57:06 -0400

    more fixes

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,5 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { User, Challenge } = require('../models');
+const { User, Challenge, Task } = require('../models');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
--- server/server.js
+++ server/server.js
@@ -6,13 +6,12 @@ const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection')
 
 const PORT = process.env.PORT || 3001;
+const app = express();
 const server = new ApolloServer({
     typeDefs,
     resolvers,
     context: authMiddleware
 });
-
-const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
