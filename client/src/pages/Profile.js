b303ae0b06adb4606d0516ea62ddcfa91ecbcd1c
MOX UFO <mox.ufo@gmail.com>
2023-07-04 10:19:38 -0400

    started the profile page and added front end mutations and queries

---
+++ client/src/pages/Profile.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../assets/css/Profile.css'
+
+export default function Profile() {
+  return (
+    <div className='profile-container'>
+        Yoooooooo
+    </div>
+  )
+}
---
+++ client/src/utils/queries.js
@@ -0,0 +1,19 @@
+import { gql } from '@apollo/client';
+
+
+
+export const QUERY_ME = gql`
+  query me {
+    me {
+      _id
+      username
+      email
+      thoughts {
+        _id
+        thoughtText
+        thoughtAuthor
+        createdAt
+      }
+    }
+  }
+`;
