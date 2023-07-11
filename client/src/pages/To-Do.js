60cfc39f891b84d4683cf82a95537eadeb56de23
MOX UFO <mox.ufo@gmail.com>
2023-07-11 14:03:39 -0400

    to-do page now displays the task according to the day and the next

--- client/src/pages/To-Do.js
+++ client/src/pages/To-Do.js
@@ -11,9 +11,79 @@ import TextField from '@mui/material/TextField';
 import '../../src/assets/css/To-Do.css'
 import auth from '../utils/auth';
 import RedirectLogin from './Redirect-Login';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime'
+import { useQuery } from '@apollo/client';
+import { QUERY_CHALLENGE } from '../utils/queries';
 
 
 const TODO = () => {
+  const {loading, error, data} = useQuery(QUERY_CHALLENGE)
+  // const challenge = data?.challenge || {} ;
+  // const tasks = challenge.tasks
+  // ;
+  // const logTask = () => {console.log( tasksy)}
+  // logTask()
+  dayjs.extend(relativeTime);
+
+  const markedDays = (frequency) => {
+    let startDate = dayjs('2023-07-01');
+    let eventDate = startDate.add(frequency, 'day');
+    let endDate = startDate.add(30, 'day');
+    let dates = [];
+  
+    while (eventDate.isBefore(endDate)) {
+      dates.push(eventDate.format('YYYY-MM-DD'));
+      eventDate = eventDate.add(frequency, 'day');
+    }
+  
+    return dates;
+  };
+  const nextDays = (frequency) => {
+    let startDate = dayjs('2023-07-01');
+    let eventDate = startDate.add(frequency + 1, 'day');
+    let endDate = startDate.add(30, 'day');
+    let dates = [];
+  
+    while (eventDate.isBefore(endDate)) {
+      dates.push(eventDate.format('YYYY-MM-DD'));
+      eventDate = eventDate.add(frequency, 'day');
+    }
+  
+    return dates;
+  };
+  
+  
+  function isDisplayed(challengeDate, task) {
+    // console.log(task);
+    const takss =[]
+    const taskDays = markedDays(task.frequency, dayjs(challengeDate))
+    const today = dayjs().format('YYYY-MM-DD')
+    if (taskDays.includes(today)) {
+      takss.push(task.taskTitle)
+    }
+    // console.log(takss);
+    return takss
+}
+function nextDisplayed(challengeDate, task) {
+  // console.log(task);
+  const takss =[]
+  const taskDays = nextDays(task.frequency, dayjs(challengeDate))
+  const today = dayjs().format('YYYY-MM-DD')
+  if (taskDays.includes(today)) {
+    takss.push(task.taskTitle)
+  }
+  // console.log(takss);
+  return takss
+}
+
+
+  
+
+ 
+  
+ 
+
 
     const customStyles = {
         '@media (max-width: 320px)' : {
@@ -63,50 +133,28 @@ const TODO = () => {
   return (
     <main className="to-do-container">
         <Paper sx={customStyles} >
-            <h1>{format(new Date(), "MM-dd-yyyy")}</h1>
-          
+            <h1>{dayjs().format('YYYY-MM-DD')}</h1>
+            {loading ? <h2>Loading...</h2> : 
+      data.challenge.tasks.map(task => {
+       const takss = isDisplayed(data.challenge.startDate, task)
+       console.log(takss);
+       return(
+        <h3>{takss}</h3>
+       )
+       })}
         </Paper>
 
-        <Paper sx={customStyles} >
-            <h2>Today's To-Do's</h2>
-            <Timeline>
-      <TimelineItem>
-        <TimelineSeparator>
-          <TimelineDot />
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent>Eat</TimelineContent>
-      </TimelineItem>
-      <TimelineItem>
-        <TimelineSeparator>
-          <TimelineDot />
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent>Code</TimelineContent>
-      </TimelineItem>
-      <TimelineItem>
-        <TimelineSeparator>
-          <TimelineDot />
-        </TimelineSeparator>
-        <TimelineContent>Sleep</TimelineContent>
-      </TimelineItem>
-    </Timeline>
-        </Paper>
 
         <Paper sx={customStyles} >
-            <h2>Journal</h2>
-            <TextField
-          id="filled-multiline-static"
-          label="Jornaul Entry"
-          multiline
-          rows={4}
-          defaultValue="Type to start a note...."
-          variant="filled"
-          sx={{
-            width: '85%',
-            paddingBottom: '15px',
-          }} 
-        />
+            <h1>{dayjs().add(1,'day').format('YYYY-MM-DD')}</h1>
+            {loading ? <h2>Loading...</h2> : 
+      data.challenge.tasks.map(task => {
+       const takss = nextDisplayed(data.challenge.startDate, task)
+       console.log(takss);
+       return(
+        <h3>{takss}</h3>
+       )
+       })}
         </Paper>
 
         
