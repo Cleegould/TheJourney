53d4f13ed4a741e6db5c3157c884a2506f396747
Clay_MM <clay.moyer.1515@gmail.com>
2023-07-12 00:15:10 -0400

    Changed the default fonts for the navbar and css, added a custom theme via materialUI, and wrapped the app inside it.

---
+++ client/src/Theme.js
@@ -0,0 +1,9 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: 'Papyrus, Roboto, Oxygen, Ubuntu, Cantarell, "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif',
+  },
+});
+
+export default theme;
