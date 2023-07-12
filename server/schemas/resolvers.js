ee3c2074ee070e34867795ef6caf221eff8e8063
MOX UFO <mox.ufo@gmail.com>
2023-07-11 20:16:59 -0400

    global delete task update

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -102,7 +102,7 @@ const resolvers = {
     removeTask: async (parent, { _id }, context) => {
       if (context.user) {
         try {
-          const deletedTask = await Task.findByIdAndDelete(_id);
+          const deletedTask = await Task.findByIdAndDelete({_id: _id});
           if (!deletedTask) {
             throw new Error('Task not found');
           }
