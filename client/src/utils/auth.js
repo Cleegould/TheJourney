d08ef08da315638732265e434eb29df3749eab47
chrisg <cleegould@gmail.com>
2023-07-13 21:40:12 -0400

    fix

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -1,5 +1,6 @@
 // use this to decode a token and get the user's information out of it
 import decode from 'jwt-decode';
+import {useNavigate } from 'react-router-dom'
 
 // create a new class to instantiate for a user
 class AuthService {
@@ -35,7 +36,8 @@ class AuthService {
   login(idToken) {
     // Saves user token to localStorage
     localStorage.setItem('id_token', idToken);
-    window.location.assign('/profile');
+    
+    //window.location.assign('/profile');
   }
 
   logout() {
